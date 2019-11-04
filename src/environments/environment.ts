eceffeabac4f5b0dc9cc00d95a9f43f29e76dd0a
tshepo.sibiya@sasfin.com <tshepo.sibiya@sasfin.com>
2019-11-04 09:37:14 +0200

    initial commit :)

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+const authBaseUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAR4Yezxk7Ao4qeFntu7tIvE7pH28Eh64Y';
+const accountBaseUrl = 'https://momentum-retail-practical-test.firebaseio.com/accounts/';
+const clientBaseUrl = 'https://momentum-retail-practical-test.firebaseio.com/clients/';
+export const environment = {
+  production: false,
+  authUrl: authBaseUrl,
+  accountsEndPoint: accountBaseUrl,
+  clientsEndPoint: clientBaseUrl
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
